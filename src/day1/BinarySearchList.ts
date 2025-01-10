031c71a301033b2589f70911c24ae290ad1067a7
Stefano Frontini <stefanofrontini75@gmail.com>
2025-01-10 11:58:55 +0100

    binary search rec

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -15,3 +15,20 @@ export default function bs_list(haystack: number[], needle: number): boolean {
     } while (low < high);
     return false;
 }
+
+function bs_list_rec(haystack: number[], needle: number): boolean {
+    if (haystack.length === 0) {
+        return false;
+    }
+
+    const high = haystack.length;
+    const m = Math.floor(high / 2);
+    const v = haystack[m];
+    if (v === needle) {
+        return true;
+    } else if (v < needle) {
+        return bs_list_rec(haystack.slice(m + 1), needle);
+    } else {
+        return bs_list_rec(haystack.slice(0, m), needle);
+    }
+}
