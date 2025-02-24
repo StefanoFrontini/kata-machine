45d2e068d20806e57b87598568572896c18c0800
Stefano Frontini <stefanofrontini75@gmail.com>
2025-02-24 16:39:08 +0100

    queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,65 @@
+type Node<T> = {
+    value: T;
+    next: Node<T> | undefined;
+};
 export default class Queue<T> {
     public length: number;
-
-    
+    private head: Node<T> | undefined;
+    private tail: Node<T> | undefined;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
+    }
+    createNode(item: T): Node<T> {
+        const n: Node<T> = {
+            value: item,
+            next: undefined,
+        };
+        return n;
     }
 
     enqueue(item: T): void {
-
-}
+        const n = this.createNode(item);
+        if (this.tail === undefined) {
+            this.head = this.tail = n;
+        } else {
+            this.tail.next = n;
+            this.tail = n;
+        }
+        this.length++;
+    }
     deque(): T | undefined {
-
-}
+        if (this.head === undefined) {
+            return undefined;
+        } else if (this.head.next === undefined) {
+            const head = this.head;
+            this.head = this.tail = undefined;
+            this.length--;
+            return head.value;
+        } else {
+            const head = this.head;
+            const next = this.head.next;
+            this.head = next;
+            this.length--;
+            return head.value;
+        }
+    }
     peek(): T | undefined {
+        if (this.head === undefined) {
+            return undefined;
+        } else {
+            return this.head.value;
+        }
+    }
+}
 
-}
-}
+const q = new Queue();
+q.enqueue(10);
+q.enqueue(20);
+q.enqueue(30);
+q.deque();
+q.enqueue(40);
+const head_number = q.peek();
+console.log("head number: ", head_number);
+console.log(JSON.stringify(q, null, 2));
--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,57 @@
+type Node<T> = {
+    value: T;
+    next: Node<T> | undefined;
+};
 export default class Stack<T> {
     public length: number;
-
-    
+    private head: Node<T> | undefined;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
+    }
+    createNode(item: T): Node<T> {
+        const newNode = {
+            value: item,
+            next: undefined,
+        };
+        return newNode;
     }
 
     push(item: T): void {
-
-}
+        const n = this.createNode(item);
+        if (this.head === undefined) {
+            this.head = n;
+        } else {
+            n.next = this.head;
+            this.head = n;
+        }
+        this.length++;
+    }
     pop(): T | undefined {
-
-}
+        if (this.head === undefined) {
+            console.log("Cannot pop, stack is empty");
+            return undefined;
+        } else {
+            const head = this.head;
+            this.head = head.next;
+            this.length--;
+            return head.value;
+        }
+    }
     peek(): T | undefined {
+        if (this.head === undefined) {
+            return undefined;
+        } else {
+            return this.head.value;
+        }
+    }
+}
 
-}
-}
+const s = new Stack();
+s.push(10);
+s.push(20);
+s.pop();
+const head = s.peek();
+console.log("head: ", head);
+console.log(JSON.stringify(s, null, 2));
