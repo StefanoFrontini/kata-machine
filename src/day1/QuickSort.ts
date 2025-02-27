838f58e74e7eb6e4f40ad5c9f502da063d129937
Stefano Frontini <stefanofrontini75@gmail.com>
2025-02-27 09:46:21 +0100

    quick sort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,47 @@
-export default function quick_sort(arr: number[]): void {
+function qs(arr: number[], lo: number, hi: number): void {
+    if (lo >= hi) {
+        return;
+    }
 
+    const pivotIdx = partition(arr, lo, hi);
+
+    qs(arr, lo, pivotIdx - 1);
+    qs(arr, pivotIdx + 1, hi);
 }
+function partition(arr: number[], lo: number, hi: number): number {
+    const pivot = arr[hi];
+
+    let idx = lo - 1;
+    for (let i = lo; i < hi; i++) {
+        if (arr[i] <= pivot) {
+            idx++;
+            const tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+    }
+    idx++;
+    arr[hi] = arr[idx];
+    arr[idx] = pivot;
+
+    return idx;
+}
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
+    // console.log(arr);
+}
+
+// export default function quick_sort(arr: number[]): number[] {
+//     if (arr.length < 2) {
+//         return arr;
+//     } else {
+//         const pivot = arr[0];
+//         const less = arr.slice(1).filter((el) => el <= pivot);
+//         const greater = arr.slice(1).filter((el) => el > pivot);
+//         return [...quick_sort(less), pivot, ...quick_sort(greater)];
+//     }
+// }
+
+// console.log(quick_sort([9, 3, 7, 4, 69, 420, 42]));
+
+// quick_sort([9, 3, 7, 4, 69, 420, 42]);
