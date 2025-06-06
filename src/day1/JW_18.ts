b7b2fcfe452492e3c04078b66f8ec26492a913e5
Stefano Frontini <stefanofrontini75@gmail.com>
2025-06-06 09:50:18 +0200

    dfs traverse graph

--- src/day1/JW_18.ts
+++ src/day1/JW_18.ts
@@ -17,4 +17,21 @@ alice.add_adjacent_vertex(bob);
 alice.add_adjacent_vertex(cynthia);
 bob.add_adjacent_vertex(cynthia);
 cynthia.add_adjacent_vertex(bob);
-console.log(alice.adjacent_vertices);
+
+function dfs_traverse(vertex: Vertex, visited_vertices = new Map()) {
+    visited_vertices.set(vertex.value, true);
+    console.log(vertex.value);
+    for (let adjacent_vertex of vertex.adjacent_vertices) {
+        if (visited_vertices.has(adjacent_vertex.value)) {
+            continue;
+        } else {
+            dfs_traverse(adjacent_vertex, visited_vertices);
+        }
+    }
+}
+console.log("alice: ");
+dfs_traverse(alice);
+console.log("bob: ");
+dfs_traverse(bob);
+console.log("cynthia: ");
+dfs_traverse(cynthia);
